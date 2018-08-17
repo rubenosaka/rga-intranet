4270ab1df9d991b1039aecf75db557e0e94fcfff
Ruben <ruben@productschool.com>
2018-08-17 11:37:54 +0200

    Remove friendship functionallity

--- plugins/rga-intranet-employee/Assets/js/employees-scripts.js
+++ plugins/rga-intranet-employee/Assets/js/employees-scripts.js
@@ -1,34 +1,47 @@
 jQuery(document).ready(function(){
-  console.log('Init Employees Scripts');
 
-  jQuery('.friendship').on('click', function(e){
+  jQuery('.friend_btn').on('click', function(e){
     e.preventDefault();
-    console.log('lets be friends!');
-
-    var cu = jQuery(this).closest('.employee').attr('data-cu');
-    var employee = jQuery(this).closest('.employee').attr('data-employee');
+    var if_friend = jQuery(this).attr('data-avatar');
+    var sel = jQuery(this).closest('.employee');
+    var cu = sel.attr('data-cu'),employee = sel.attr('data-employee'), name = sel.attr('data-name'), avatar = sel.attr('data-avatar'), ac;
     jQuery(this).find('i').toggleClass('fa-bookmark-o fa-bookmark');
-
-    add_coworker_as_friend(cu, employee);
+    if(if_friend == 'true'){
+      ac = 'add';
+      jQuery(this).attr('data-avatar','false');
+    }else{
+      ac = 'remove';
+      jQuery(this).attr('data-avatar','true');
+    }
+    add_coworker_as_friend(cu, employee, name, avatar, ac);
   });
 
 });
 
-function add_coworker_as_friend(cu_id, id){
-
-    var ajaxurl = 'http://'+window.location.host+'/intranet/wp-admin/admin-ajax.php';
+function add_coworker_as_friend(cu_id, id, name, avatar, ac){
+  var func, html;
+  var ajaxurl = 'http://'+window.location.host+'/intranet/wp-admin/admin-ajax.php';
+  if (ac == 'add'){
+    func = 'add_friend';
+    html = '<div class="row"><div class="col-sm-12"><img src="'+avatar+'" class="avatar avatar-sm" />'+name+'<h5>Have just added to your friendship list</h5></div></div>';
+  }else{
+    func = 'remove_friend';
+    html = '<div class="row"><div class="col-sm-12"><img src="'+avatar+'" class="avatar avatar-sm" />'+name+'<h5>Have just removed from your friendship list</h5></div></div>';
+  }
 
    jQuery.ajax({
-   url: ajaxurl,
-   type: 'POST',
-   data: {action : 'test_function','cu_id': cu_id, 'id' : id  },
-     success: function(response) {
-     console.log(response);
-     console.log("SUCCESS!");
-    },
-     error: function(data) {
-     console.log("FAILURE");
+     url: ajaxurl,
+     type: 'POST',
+     data: {action : func,'cu_id': cu_id, 'id' : id  },
+       success: function(response) {
+       jQuery('.notification_content').html(html);
+       //jQuery('.notification_content').html(response);
+       jQuery('#push_notification').fadeIn();
 
-    }
+      },
+       error: function(data) {
+       console.log("Something goes wrong. Contact with the adeministrator to solve it.");
+
+      }
   });
 }
--- plugins/rga-intranet-employee/includes/coworkers.php
+++ plugins/rga-intranet-employee/includes/coworkers.php
@@ -2,6 +2,12 @@
 
 
   function coworkers($id, $employee){
+    $fls = get_field('my_friends', $employee);
+    $flf = array();
+
+    foreach ($fls as $fl){$flf[]= $fl->ID;}
+
+    print_r($flf);
 
     $args  = array(
     	'numberposts'	=> 3,
@@ -22,14 +28,21 @@
     while( $the_query->have_posts() ) : $the_query->the_post();
 
 
+        if(in_array(get_the_ID(), $flf)){$class = 'friend'; }else{$class = 'coworker';}
+
     		?>
-        <article data-cu="<?php echo $employee; ?>" data-employee="<?php echo get_the_ID(); ?>" class="col-lg-4 employee employee-<?php echo get_the_ID(); ?>">
+        <article data-cu="<?php echo $employee; ?>" data-employee="<?php echo get_the_ID(); ?>" data-name="<?php the_title(); ?>" data-avatar="<?php echo get_field('avatar'); ?>" class="col-lg-4 employee employee-<?php echo get_the_ID(); echo ' '.$class;?>">
           <div class="imgNews">
-            <a class="avatar-med" href="<?php the_permalink(); ?>" style="background-image:url(<?php echo get_field('avatar'); ?>);"></a>
+            <a class="avatar-med avatar" href="<?php the_permalink(); ?>" style="background-image:url(<?php echo get_field('avatar'); ?>);"></a>
           </div>
           <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
           <div class="actions">
-            <a href="" class="friendship"><i class="fa fa-bookmark-o" aria-hidden="true"></i></a>
+            <?php if(in_array(get_the_ID(), $flf)){ ?>
+              <a href="" class="friend_btn" data-friend="true"><i class="fa fa-bookmark" aria-hidden="true"></i></a>
+            <?php }else{ ?>
+              <a href="" class="friend_btn" data-friend="false"><i class="fa fa-bookmark-o" aria-hidden="false"></i></a>
+            <?php } ?>
+
 
             <a href="" class="talk"><i class="fa fa-comments-o" aria-hidden="true"></i></a>
           </div>
@@ -61,14 +74,40 @@
 * AJAX EMPLOYEES CALLS
 *
 **/
+add_action( 'wp_ajax_nopriv_add_friend',  'add_friend' );
+add_action( 'wp_ajax_add_friend','add_friend' );
 
-function test_function() {
-      $cu_id = $_GET['cu_id'];
-      $id = $_GET['id'];
-      echo  $cu_id;
-      echo  $id;
+function add_friend() {
+      $cu_id = $_POST['cu_id'];
+      $id = $_POST['id'];
+      $mf = get_field('my_friends', $cu_id);
 
-      update_field('my_friends', array($id), $id);
+      if( !is_array($mf) ):
+      	$mf = array();
+      endif;
+      array_push($mf, $id);
+
+      update_field('my_friends', $mf, $cu_id);
+
+      die();
    }
-add_action( 'wp_ajax_nopriv_test_function',  'test_function' );
-add_action( 'wp_ajax_test_function','test_function' );
+
+   add_action( 'wp_ajax_nopriv_remove_friend',  'remove_friend' );
+   add_action( 'wp_ajax_remove_friend','remove_friend' );
+
+   function remove_friend() {
+         $cu_id = $_POST['cu_id'];
+         $id = $_POST['id'];
+         $fls = get_field('my_friends', $cu_id);
+
+         $flf = array();
+         foreach ($fls as $fl){$flf[]= $fl->ID;}
+         print_r($flf);
+         if (($key = array_search($id, $flf)) !== false) {
+           unset($flf[$key]);
+         }
+         print_r($flf);
+         update_field('my_friends', $flf, $cu_id);
+
+         die();
+      }
